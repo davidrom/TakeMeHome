0b608c7d2ce90ec82fc59440db454360e0c7f7d9
Romain David <romain.box@gmail.com>
2014-09-26 11:15:08 -0700

    Initial Commit

---
+++ TakeMeHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TakeMeHome
+//
+//  Created by Romain on 9/26/14.
+//  Copyright (c) 2014 Romain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
