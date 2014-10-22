dec51d2a27c14823e64933bbe4431e112d36299a
Romain David <romain.box@gmail.com>
2014-10-22 08:17:26 -0700

    finalized deep link url with uber product id

--- TakeMeHome/ViewController.swift
+++ TakeMeHome/ViewController.swift
@@ -9,59 +9,34 @@
 import UIKit
 import CoreLocation
 
-// class ViewController: UIViewController {
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var homeAddressTextField: UITextField!
 
-    // var manager:CLLocationManager!
     let locationManager = CLLocationManager()
     var currentLocation = Location()
+    var uberProductId = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Temporary assign Home Address and lat and long
+        // Temporary assign Home Address
         homeAddressTextField.text = "300 3rd Street, San Francisco, CA 94107"
         
-        // Core Location
-//        manager = CLLocationManager()
-//        manager.delegate = self
-//        manager.desiredAccuracy = kCLLocationAccuracyBest
-//        manager.requestWhenInUseAuthorization()
-//        manager.startUpdatingLocation()
-
         self.locationManager.requestWhenInUseAuthorization()
         if (CLLocationManager.locationServicesEnabled()) {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
-
     }
 
-    // Get the current location
+    // Get user's current location
     func locationManager(manager:CLLocationManager!, didUpdateLocations locations:[AnyObject]!) {
-        
-        // var userLocation:CLLocation = locations[0] as CLLocation
         var locValue:CLLocationCoordinate2D = manager.location.coordinate
         currentLocation.latitude = Float(locValue.latitude)
         currentLocation.longitude = Float(locValue.longitude)
-        
-        // var latitude:CLLocationDegrees = userLocation.coordinate.latitude
-        // var longitude:CLLocationDegrees = userLocation.coordinate.longitude
-        
-        // println("latitude = \(latitude)")
-        
-        // println("locations = \(locValue.latitude) \(locValue.longitude)")
-
-        
-        // currentLocation.latitude = Float(latitude)
-        // currentLocation.longitude = Float(longitude)
-        
-
-        
     }
     
     func locationManager(manager:CLLocationManager, didFailWithError error:NSError)
@@ -89,12 +64,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
             else {
                 let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-                println(jsonResult)
-                // println("product id = ")
-                // let myproduct = jsonResult(["products"]["product_id"].array)
-                var productId = jsonResult["products"]?[0]["product_id"]
-                // println(jsonResult["products"]?[0]["product_id"])
-                println(productId)
+                self.uberProductId = jsonResult["products"]?[0]["product_id"] as NSString
             }
         })
         task.resume()
@@ -121,32 +91,35 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if let placemark = placemarks?[0] as? CLPlacemark {
                 var homeLatitude = placemark.location.coordinate.latitude
                 var homeLongitude = placemark.location.coordinate.longitude
-                //self.mapView.addAnnotation(MKPlacemark(placemark: placemark))
                 println("Home Latidude: \(homeLatitude)")
                 println("Home Longitude: \(homeLongitude)")
 
                 var encodedAddressString:String = homeAddress.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
-                // println("URL-encoded Home Address: \(encodedAddressString)")
                 
                 if encodedAddressString != ""
                 {
                     encodedAddressString = encodedAddressString.stringByReplacingOccurrencesOfString(",", withString: "%2C", options: NSStringCompareOptions.LiteralSearch, range: nil)
                 }
-                
-                // println("\(homeAddress.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)")
-                
+                                
                 println("home address = \(homeAddress)")
                 println("encoded home address = \(encodedAddressString)")
                 
-                var dropoffAddress:String = "Home%20(" + "\(encodedAddressString)" + ")"
+                // Wait until the app hears back from Uber
+                while self.uberProductId.isEmpty {
+                    
+                }
                 
+                var dropoffAddress:String = "Home%20(" + "\(encodedAddressString)" + ")"
+    
                 var uberDeepLink:String = "uber://?action=setPickup&pickup[latitude]=\(self.currentLocation.latitude)&pickup[longitude]=\(self.currentLocation.longitude)"
-                uberDeepLink += "&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103"
+                // uberDeepLink += "&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103"
+                // uberDeepLink += "&pickup[nickname]=Current Location&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103"
                 uberDeepLink += "&dropoff[latitude]=\(homeLatitude)&dropoff[longitude]=\(homeLongitude)"
                 uberDeepLink += "&dropoff[nickname]=\(dropoffAddress)&dropoff[formatted_address]=\(encodedAddressString)"
-                uberDeepLink += "&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d"
+                uberDeepLink += "&product_id=\(self.uberProductId)"
                 
                 println("uberDeepLink = \(uberDeepLink)")
+                println("product id = \(self.uberProductId)")
                 
                 let url = NSURL(string: uberDeepLink)
 
@@ -158,54 +131,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         })
         
         
-//        var encodedAddressString = homeAddress.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
-//        
-        
-
-        
-        
-        
-        // If Uber is installed on the device, then launch the app
-        // var uberDeepLink = "uber://?action=setPickup&pickup=my_location&dropoff[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103";
-        // var uberDeepLink = "uber://?action=setPickup&pickup=my_location&dropoff[latitude]=37.771026&dropoff[longitude]=-122.404051&dropoff[formatted_address]=699%208th%20St%2C%20San%20Francisco%2C%20CA%2094103&product_id=b5e74e96-5d27-4caf-83e9-54c030cd6ac5"
-        
-        // From Uber doc
-        // uber://?action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d
-        
-        
         // If Uber app is not installed on the device
         // uberDeepLink = "https://www.uber.com/invite/drv52"
     
-        
-        
-
-        
-
-        
-        
-        
-        
-        
-        // [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"itms://itunes.com/apps/Reactors"]]
-        
-        /*
-        
-        NSString *urlAddress = @”http://www.google.com”
-        
-        //Create a URL object.
-        NSURL *url = [NSURL URLWithString:urlAddress];
-        
-        //URL Requst Object
-        NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
-        
-        //Load the request in the UIWebView.
-        [webView loadRequest:requestObj];
-   
-        */
-
     }
     
-
     
 
 }
